a9363dfbf73fbf181b78b9fc3adbb567b13ca89d
alex <alexsidorkin567@gmail.com>
2024-07-15 20:27:24 +0300

    изменение маршрута app_hom ,создание файла calendar.js,изменения в шаблоне base.html.twig и index.html.twig

--- my_project_directory/src/userManager/Controller/HomeController.php
+++ my_project_directory/src/userManager/Controller/HomeController.php
@@ -4,13 +4,31 @@ namespace App\userManager\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\TodoService;
+use GuzzleHttp\Client;
 
 class HomeController extends AbstractController
 {
+    private $todoService;
+
+    public function __construct(TodoService $todoService) 
+    {
+        $this->todoService = $todoService;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', []);
+            $user = $this->getUser();
+            if (!$user) {
+                throw $this->createAccessDeniedException('User is not authenticated.');
+            }
+            $accessToken = $user->getTodoAccessToken();
+            $tasks = $this->todoService->getTasks($accessToken);
+    
+            return $this->render('home/index.html.twig', [
+                'tasks' => $tasks,
+            ]);
+        }
     }
-}
