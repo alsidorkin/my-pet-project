646d7a1447f59efd38fe2248b2d368679787c90b
alex <alexsidorkin567@gmail.com>
2024-08-01 09:52:13 +0300

    создание CommentController и маршрутов app_comment,get_comment,add_comment,update_comment,delete_comment

---
+++ my_project_directory/src/api/Controller/CommentController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\api\Controller;
+
+use App\Entity\Comment;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+    private $entityManager;
+    private $serializer;
+
+    public function __construct(EntityManagerInterface $entityManager, SerializerInterface $serializer)
+    {
+        $this->entityManager = $entityManager;
+        $this->serializer = $serializer;
+    }
+
+    #[Route('/api/comment', name: 'app_comment', methods: ['POST'])]
+    public function getComments(CommentRepository $commentRepository): JsonResponse
+    {
+        $comments = $commentRepository->findAll();
+   
+        $data = $this->serializer->normalize($comments, null, ['groups' => 'comment:read']);
+        return new JsonResponse($data);
+
+    }
+
+    #[Route('/api/comment/{id}', name: 'get_comment', methods: ['GET'])]
+    public function getComment(Comment $comment): JsonResponse
+    {
+        $data = $this->serializer->normalize($comment, null, ['groups' => 'comment:read']);
+        return new JsonResponse($data);
+    }
+
+    #[Route('/api/comment/add', name: 'add_comment', methods: ['POST'])]
+    public function createComment(Request $request): JsonResponse
+    {
+        $data = $request->getContent();
+        $comment = $this->serializer->deserialize($data, Comment::class, 'json');
+
+        $this->entityManager->persist($comment);
+        $this->entityManager->flush();
+
+        return new JsonResponse(['status' => 'Comment added'], JsonResponse::HTTP_CREATED);
+    }
+
+    #[Route('/api/comment/update/{id}', name: 'update_comment', methods: ['PUT'])]
+    public function updateComment(Request $request, Comment $comment): JsonResponse
+    {
+        $data = $request->getContent();
+        $this->serializer->deserialize($data, Comment::class, 'json', ['object_to_populate' => $comment]);
+
+        $this->entityManager->flush();
+
+        return new JsonResponse(['status' => 'Comment updated']);
+    }
+
+    #[Route('/api/comment/delete/{id}', name: 'delete_comment', methods: ['DELETE'])]
+    public function deleteComment(Comment $comment): JsonResponse
+    {
+        $this->entityManager->remove($comment);
+        $this->entityManager->flush();
+
+        return new JsonResponse(['status' => 'Comment deleted']);
+    }
+}
